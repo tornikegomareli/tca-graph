f911b3d4797b4a026a37747ac4d622392a6a3e01
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2026-04-26 04:29:01 +0400

    Bump version to 0.3.0

--- Sources/TCAGraphCLI/CLI.swift
+++ Sources/TCAGraphCLI/CLI.swift
@@ -176,7 +176,7 @@ struct CLI {
 
     let iso = ISO8601DateFormatter()
     let graph = Graph(
-      generator: GeneratorInfo(name: "tca-graph", version: "0.2.0"),
+      generator: GeneratorInfo(name: "tca-graph", version: "0.3.0"),
       generatedAt: iso.string(from: Date()),
       source: Source(
         rootPath: rootURL.path,
